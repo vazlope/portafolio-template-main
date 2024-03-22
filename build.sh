9727a6dabf016bfd5d9e97c5f2b599780fa237a0
vazlope <pablo.vazquez.lopez@protonmail.com>
2024-03-23 00:46:29 +0100

    build change

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-python3 -m venv .venv
-source .venv/bin/activate
+# python3 -m venv .venv
+# source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-rm -rf public
+# rm -rf public
 reflex init
 reflex export --frontend-only
-unzip frontend.zip -d public
-rm -f frontend.zip
-deactivate
+#unzip frontend.zip -d public
+# rm -f frontend.zip
+# deactivate
